840a212a4e0bbb7ce3c4666f1d5cebd7d3b107f3
Suvankar Sen <suvankar.sen@infosys.com>
2023-06-22 15:52:58 +0530

    Initial Commit

---
+++ src/main/java/com/lkq/fetchipaddress/FetchIpAddressApplication.java
@@ -0,0 +1,13 @@
+package com.lkq.fetchipaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchIpAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchIpAddressApplication.class, args);
+	}
+
+}
